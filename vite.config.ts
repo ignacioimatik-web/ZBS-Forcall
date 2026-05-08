19c2066191c7050708510ddaf289e226e83aaa0f
ignacioimatik-web <ignacio.imatik@gmail.com>
2026-05-08 02:11:25 +0200

    fix(build): force new chunk names to invalidate cache

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,15 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        rollupOptions: {
+          output: {
+            entryFileNames: 'assets/[name]-[hash]-v2.js',
+            chunkFileNames: 'assets/[name]-[hash]-v2.js',
+            assetFileNames: 'assets/[name]-[hash]-v2[extname]'
+          }
+        }
       }
     };
 });
